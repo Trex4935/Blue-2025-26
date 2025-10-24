46148710e79d1e955f5b4251d77dc13edb4f984e
Xavier L <xl.lan.gamerz@gmail.com>
2025-10-24 19:16:32 -0400

    Drivetrain file created

---
+++ Blue 2025/26/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,31 @@
+package main.java.frc.robot.subsystems;
+
+import java.nio.channels.WritePendingException;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+    public WPI_TalonSRX motor1;
+    public WPI_TalonSRX motor2;
+    public WPI_TalonSRX motor3;
+    public WPI_TalonSRX motor4;
+    public WPI_TalonSRX motor5;
+    public WPI_TalonSRX motor6;
+    public WPI_TalonSRX motor7;
+    public WPI_TalonSRX motor8;
+
+    public Drivetrain() {
+        motor1 = new WPI_TalonSRX(11);
+        motor2 = new WPI_TalonSRX(12);
+        motor3 = new WPI_TalonSRX(13);
+        motor4 = new WPI_TalonSRX(14);
+        motor5 = new WPI_TalonSRX(21);
+        motor6 = new WPI_TalonSRX(22);
+        motor7 = new WPI_TalonSRX(23);
+        motor8 = new WPI_TalonSRX(24);
+
+    }
+}
